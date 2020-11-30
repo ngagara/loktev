33e40bdc570e5649bf55ee1cb4c5f4d7671af36a
Nikolay Gagara <ggn.f763@gmail.com>
2020-12-01 00:33:30 +0300

    поправил вёрстку

--- src/index.js
+++ src/index.js
@@ -1,59 +1,60 @@
-import '/index.scss';
-import 'swiper/swiper-bundle.css';
+import "/index.scss";
+import "swiper/swiper-bundle.css";
 
-import Swiper from 'swiper/bundle';
+import Swiper from "swiper/bundle";
 
 new Swiper(".swiper-container", {
-    // init: false,
-    loop: true,
-    grabCursor: true,
-    loopedSlides: 1,
-    autoHeight: true,
-    slidesPerGroup: 1,
-    centeredSlides: true,
-    breakpoints: {
-      320: {
-        slidesPerView: 1.1,
-        spaceBetween: 8,
-      },
-      375: {
-        slidesPerView: 1.3,
-        spaceBetween: 8,
-      },
-      425: {
-        slidesPerView: 1.4,
-        spaceBetween: 8,
-      },
-      460: {
-        slidesPerView: 1.6,
-        spaceBetween: 8,
-      },
-      500: {
-        slidesPerView: 1.7,
-        spaceBetween: 8,
-      },
-      600: {
-        slidesPerView: 2.0,
-        spaceBetween: 8,
-      },
-      767: {
-        slidesPerView: 2.5,
-        spaceBetween: 12,
-      },
+  // init: false,
+  loop: true,
+  grabCursor: true,
+  loopedSlides: 1,
+  autoHeight: true,
+  slidesPerGroup: 1,
+  centeredSlides: true,
+  breakpoints: {
+    320: {
+      slidesPerView: 1.1,
+      spaceBetween: 8,
     },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-    keyboard: {
-        enabled: true,
-        onlyInViewport: true,
-        pageUpDown:true,
-    } 
-  });
-
-
-
-  
-  
-  
+    375: {
+      slidesPerView: 1.3,
+    },
+    425: {
+      slidesPerView: 1.4,
+    },
+    460: {
+      slidesPerView: 1.6,
+    },
+    500: {
+      slidesPerView: 1.7,
+    },
+    600: {
+      slidesPerView: 2.0,
+    },
+    767: {
+      slidesPerView: 2.5,
+      spaceBetween: 12,
+    },
+    860: {
+      slidesPerView: 2.8,
+    },
+    1024: {
+      slidesPerView: 3.2,
+    },
+    1210: {
+      slidesPerView: 3.6,
+    },
+    1280: {
+      slidesPerView: 4,
+    },
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+    pageUpDown: true,
+  },
+});
