e663dae7b7553b23a581f8a3d64ad477db83fd60
Nikolay Gagara <ggn.f763@gmail.com>
2020-12-02 01:22:05 +0300

    добавлен функционал навигации

--- src/index.js
+++ src/index.js
@@ -2,5 +2,19 @@ import "/index.scss";
 import "swiper/swiper-bundle.css";
 import "./scripts/swiper";
 
+const headerNav = document.querySelector(".header__nav");
+const headerNavOpen = document.querySelector(".header__open");
+const sketchsLink = document.getElementById("SKETCHES");
+const singUpLink = document.getElementById("SIGN_UP");
 
+headerNavOpen.addEventListener("click", (event) => {
+    headerNav.classList.toggle("header__nav_off");
+  });
 
+headerNav.addEventListener("click", (event) => {
+  if (event.target == sketchsLink) {
+    document.querySelector(".main").scrollIntoView({ block: "start", behavior: "smooth" });
+  } else if (event.target == singUpLink) {
+    document.querySelector(".footer").scrollIntoView({ block: "start", behavior: "smooth" });
+  }
+});
