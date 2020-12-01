c92cde77881cf636268ab6074c8edee4a83733d9
Nikolay Gagara <ggn.f763@gmail.com>
2020-12-01 23:22:48 +0300

    вёрстка формы и адаптация её

--- src/index.js
+++ src/index.js
@@ -1,60 +1,6 @@
 import "/index.scss";
 import "swiper/swiper-bundle.css";
+import "./scripts/swiper";
 
-import Swiper from "swiper/bundle";
 
-new Swiper(".swiper-container", {
-  // init: false,
-  loop: true,
-  grabCursor: true,
-  loopedSlides: 1,
-  autoHeight: true,
-  slidesPerGroup: 1,
-  centeredSlides: true,
-  breakpoints: {
-    320: {
-      slidesPerView: 1.1,
-      spaceBetween: 8,
-    },
-    375: {
-      slidesPerView: 1.3,
-    },
-    425: {
-      slidesPerView: 1.4,
-    },
-    460: {
-      slidesPerView: 1.6,
-    },
-    500: {
-      slidesPerView: 1.7,
-    },
-    600: {
-      slidesPerView: 2.0,
-    },
-    767: {
-      slidesPerView: 2.5,
-      spaceBetween: 12,
-    },
-    860: {
-      slidesPerView: 2.8,
-    },
-    1024: {
-      slidesPerView: 3.2,
-    },
-    1210: {
-      slidesPerView: 3.6,
-    },
-    1280: {
-      slidesPerView: 4,
-    },
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  keyboard: {
-    enabled: true,
-    onlyInViewport: true,
-    pageUpDown: true,
-  },
-});
+
---
+++ src/scripts/swiper.js
@@ -0,0 +1,62 @@
+import Swiper from "swiper/bundle";
+
+export const mySwiper = new Swiper(".swiper-container", {
+    // init: false,
+    loop: true,
+    grabCursor: true,
+    loopedSlides: 1,
+    autoHeight: true,
+    slidesPerGroup: 1,
+    centeredSlides: true,
+    breakpoints: {
+      320: {
+        slidesPerView: 1.1,
+        spaceBetween: 8,
+      },
+      375: {
+        slidesPerView: 1.2,
+      },
+      425: {
+        slidesPerView: 1.4,
+      },
+      460: {
+        slidesPerView: 1.6,
+      },
+      500: {
+        slidesPerView: 1.7,
+      },
+      600: {
+        slidesPerView: 2.0,
+      },
+      767: {
+        slidesPerView: 2.5,
+        spaceBetween: 12,
+      },
+      860: {
+        slidesPerView: 2.8,
+      },
+      1024: {
+        slidesPerView: 3.2,
+      },
+      1210: {
+        slidesPerView: 3.6,
+      },
+      1280: {
+        slidesPerView: 4,
+      },
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    keyboard: {
+      enabled: true,
+      onlyInViewport: true,
+      pageUpDown: true,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'progressbar',
+    },
+  });
+  
